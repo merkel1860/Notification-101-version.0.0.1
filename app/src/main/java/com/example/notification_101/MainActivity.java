8cc4bd8dc6b3d33bdc3f1f490fa3fd9825e0b557
merkel1860 <merkelmller@yandex.ru>
2021-05-23 19:10:22 -0400

    Notification App. For now, our notification has a action buttton  with which we are able to reach a new activity_alert_details.xml depicting the whereabouts of the notification.

--- app/src/main/java/com/example/notification_101/MainActivity.java
+++ app/src/main/java/com/example/notification_101/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.core.app.NotificationManagerCompat;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -32,11 +34,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayNotification() {
+        // Create an explicit intent for an Activity in your app
+        Intent intent = new Intent(this, AlertDetails.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.black_woman)
                 .setContentTitle(noTextTitle)
                 .setContentText(noTextContent)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
         notificationManagerCompat  = NotificationManagerCompat.from(this);
         notificationManagerCompat.notify(notificationID,builder.build());
     }
